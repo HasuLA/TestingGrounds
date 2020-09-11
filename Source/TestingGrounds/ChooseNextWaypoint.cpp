71b1c729dd4f22efc4b7c7d24983f9eccd82f310
HasuLA <legendalive.hasu@gmail.com>
2020-09-11 15:36:04 +0300

    Started AI transform from BP to C++

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+
+class BTNode;
+class BlackboardComponent;
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
+{
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+
+	return EBTNodeResult::Succeeded;
+}
+
+
